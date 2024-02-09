89bfd787aa9c28f911c02421ca4ca7fe13c15b92
Andrew Read <andrew81244@outlook.com>
2024-02-09 23:20:09 +0800

    developed to testing stage

--- KMTronic.f
+++ KMTronic.f
@@ -1,19 +1,60 @@
 \ Use the serial port (COM) functionality of VFX Forth to communicate with a KMTronic USB Relay controller
 \ Windows command line tools 
-\	mode
-\	chgport
+\ mode
+\ chgport
 
-include C:\MPE\VfxForth\Lib\Win32\Genio\serialbuff
+ include C:\MPE\VfxForth\lib\Win32\Genio\serialbuff.fth
+\ this does not include properly in Win64
 
-serdev: COM_KMT
-\ prepare an instance of a VFX Forth generic I/O driver
+: connection_string ( com_port baud -- c-addr u)
+\ create a VFX COM port connection string
+\ the string is stored in the PAD and will be overwritten when the PAD is used again
+	<#  
+	s"  parity=N data=8 stop=1" HOLDS
+	( baud) 0 #s 2drop
+	s"  baud=" HOLDS
+	dup 10 < if
+		':' HOLD	
+		( com_port) 0 # 
+		s" COM" HOLDS
+	else
+		( com_port) 0 # #
+		s" \\.\COM" HOLDS
+	then	
+	#>
+;
 
-s" COM8: baud=115200 parity=N data=8 stop=1"  7 ( legacy_setting) COM_KMT open-gio . . CR
-\ prepare a suitable connection string
-\ open the port
+ serdev: COM_KMT 
+ \ prepare an instance of a VFX Forth generic I/O driver
 
-s" command_protocol_example" COM_KMT write-gio . CR
-\ send a string over the serial port to the SQM
+ create KMTronic_protocol
+ 255 c, 0 c, 0 c,
+ \ protocol binary string of 3 bytes
+ 
+: add-relays ( com_port --)
+	9600 ( com_port baud) connection_string 7 COM_KMT ( c-addr u attribs sid) open-gio ( sid ior) ABORT" Failed to open KMTronic COM port"
+;
 
-COM_KMT close-gio . CR
-\ close the serial port
+: remove-relays 
+	COM_KMT close-gio ABORT" Failed to close KMTronic COM port"
+;
+
+: switch-relay ( f r --)
+\ f = -1 on; 0 off
+\ r = relay number
+	KMTronic_protocol 1 + swap c!
+	KMTronic_protocol 2 + if 1 else 0 then swap c!
+	KMTronic_protocol 3 COM_KMT ( addr n sid) write-gio ( ior) ABORT" Failed to write KMTronic COM port"
+;
+
+: relay-on ( r --)
+ -1 swap switch-relay
+;
+
+: relay-off ( r --)
+ 0 swap switch-relay
+;
+
+
+	
+
