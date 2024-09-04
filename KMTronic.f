3b164b1a01c0cafa7f13f3ffab3a1c77b4a2695b
Andrew Read <andrew81244@outlook.com>
2024-09-04 21:03:24 +0900

    factoring - requires testing

--- KMTronic.f
+++ KMTronic.f
@@ -1,46 +1,20 @@
 \ Use the serial port (COM) functionality of VFX Forth to communicate with a KMTronic USB Relay controller
-\ Windows command line tools 
-\ mode
-\ chgport
+\ requires VFX32serial.f
 
-\ local note: PC Bidmead has the KMTronic relay box on COM6
+\ prepare an instance of a VFX Forth generic I/O driver
+ serdev: sid_KMTronic
 
- include C:\MPE\VfxForth\lib\Win32\Genio\serialbuff.fth
-\ this does not include properly in Win64
-
-: connection_string ( com_port baud -- c-addr u)
-\ create a VFX COM port connection string
-\ the string is stored in the PAD and will be overwritten when the PAD is used again
-	<#  
-	s"  parity=N data=8 stop=1" HOLDS
-	( baud) 0 #s 2drop
-	s"  baud=" HOLDS
-	dup 10 < if
-		':' HOLD	
-		( com_port) 0 # 
-		s" COM" HOLDS
-	else
-		( com_port) 0 # #
-		s" \\.\COM" HOLDS
-	then	
-	#>
+: add-relays ( com_port --)
+	9600 sid_KMTronic ( com_port baud) open-serial
 ;
 
- serdev: COM_KMT 
- \ prepare an instance of a VFX Forth generic I/O driver
+: remove-relays 
+	sid_KMTronic close-serial
+;
 
  create KMTronic_protocol
  255 c, 0 c, 0 c,
  \ protocol binary string of 3 bytes
- 
-: add-relays ( com_port --)
-	9600 ( com_port baud) connection_string 7 COM_KMT ( c-addr u attribs sid) open-gio ( sid ior) 
-	ABORT" Failed to open KMTronic COM port" drop
-;
-
-: remove-relays 
-	COM_KMT close-gio ABORT" Failed to close KMTronic COM port"
-;
 
 : switch-relay ( f r --)
 \ f = -1 on; 0 off
