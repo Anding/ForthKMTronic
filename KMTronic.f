7d348050b73bec84470383bf3d69533ab6c8674c
Andrew Read <andrew81244@outlook.com>
2024-10-22 06:38:53 +0800

    updated to used CommandString

--- KMTronic.f
+++ KMTronic.f
@@ -1,30 +1,30 @@
 \ Use the serial port (COM) functionality of VFX Forth to communicate with a KMTronic USB Relay controller
 NEED serial
+NEED CommandStrings
 
 \ COM port number, to revise on the local machine
 	6 value KMTronic.COM
 
 \ prepare an instance of a VFX Forth generic I/O driver
-	serdev: sid_KMTronic
+	serdev: KMTronic.sid
 
 : add-relays ( --)
-	KMTronic.COM 9600 sid_KMTronic ( com_port baud) open-serial
+	KMTronic.COM 9600 ( com_port baud) KMTronic.sid open-serial
 ;
 
 : remove-relays 
-	sid_KMTronic close-serial
+	KMTronic.sid close-serial
 ;
 
- create KMTronic_protocol
- 255 c, 0 c, 0 c,
+ create KMTronic.command
+ 3 allot
  \ protocol binary string of 3 bytes
 
 : switch-relay ( f r --)
 \ f = -1 on; 0 off
 \ r = relay number
-	KMTronic_protocol 1 + c!
-	if 1 else 0 then KMTronic_protocol 2 + c!
-	KMTronic_protocol 3 sid_KMTronic ( addr n sid) write-gio ( ior) ABORT" Failed to write KMTronic COM port"
+	KMTronic.command << 0xff | ( r) | ( f) if 1 else 0 then | >>
+ 	( addr 3) KMTronic.sid ( addr n sid) write-gio ( ior) ABORT" Failed to write KMTronic COM port"
 ;
 
 : relay-on ( r --)
